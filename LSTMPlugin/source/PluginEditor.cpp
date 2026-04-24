b4cad5be0c52a10be8518f60c8ada05701d921ea
Corius77 <121941546+Corius77@users.noreply.github.com>
2026-04-24 18:23:13 +0200

    some changes

---
+++ LSTMPlugin/source/PluginEditor.cpp
@@ -0,0 +1,30 @@
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
+    : AudioProcessorEditor (&p), processorRef (p)
+{
+    juce::ignoreUnused (processorRef);
+
+    setSize (400, 300);
+}
+
+AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+}
+
+void AudioPluginAudioProcessorEditor::resized()
+{
+
+}
